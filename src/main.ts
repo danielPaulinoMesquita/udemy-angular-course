88f4dfb9bc2ddb63793963a994e099b6029d78a5
Daniel Paulino <daniel-paulino@hotmail.com>
2024-12-20 17:08:20 -0300

    2- Added the way for showing the HeaderComponent

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {HeaderComponent} from "./app/header.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+// bootstrapApplication(HeaderComponent)
+//   .catch((err) => console.error(err)); //  this another way to show your recent created component, but it's not recommended because not follows the correct pattern like a tree way
