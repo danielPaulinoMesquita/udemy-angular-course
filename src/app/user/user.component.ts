2d1e516c2fbc31f87814bf61bcda2e88dca89754
Daniel Paulino <daniel-paulino@hotmail.com>
2024-12-30 20:30:38 -0300

    16- Adding generic type to EventEmitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,16 +14,16 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string
   @Input({required: true}) name!: string
-  //@Output() select = new EventEmitter()
+  @Output() select = new EventEmitter<string>() // Adding a type, ensure that value passed to select is string
 
   // this output unlike signal, it is another thing
-  select = output<string>()
+  // select = output<string>()
 
   get imagePath() {
     return "assets/users/" + this.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.id); // If we pass a number to select, it shows up a warning error because new EventEmitter<string>
   }
 }
