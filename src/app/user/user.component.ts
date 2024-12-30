86dcd806740b75c9a44be0bbbb58b40960b74643
Daniel Paulino <daniel-paulino@hotmail.com>
2024-12-30 00:08:34 -0300

    13- Transform the @Inputs to Signal inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, input, Input} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -11,12 +11,22 @@ import {NgOptimizedImage} from "@angular/common";
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string
-  @Input({required: true}) name!: string
+  // @Input({required: true}) avatar!: string
+  // @Input({required: true}) name!: string
 
-  get imagePath() {
-    return "assets/users/" + this.avatar;
+  // this way to declare only allows read, so it's read only
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/'+ this.avatar();
+  })
+
+  // get imagePath() {
+  //   return "assets/users/" + this.avatar;
+  // }
+
+  onSelectUser() {
+
   }
-
-  onSelectUser() {}
 }
