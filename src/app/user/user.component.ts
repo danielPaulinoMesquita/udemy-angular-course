b581add26ed0b5f2f27e5cb3716b744c8035b419
Daniel Paulino <daniel-paulino@hotmail.com>
2024-12-30 20:19:35 -0300

    15- Change Decorator Output to output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, EventEmitter, input, Input, Output} from '@angular/core';
+import {Component, computed, EventEmitter, input, Input, output, Output} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -14,14 +14,16 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string
   @Input({required: true}) name!: string
-  @Output() select = new EventEmitter()
+  //@Output() select = new EventEmitter()
+
+  // this output unlike signal, it is another thing
+  select = output<string>()
 
   get imagePath() {
     return "assets/users/" + this.avatar;
   }
 
   onSelectUser() {
-    console.log("Teste")
     this.select.emit(this.id);
   }
 }
