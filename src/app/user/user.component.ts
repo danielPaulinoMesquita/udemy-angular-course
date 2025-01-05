faec529216a6415f5ba48c24a3cfeb3ca41ef95f
Daniel Paulino <daniel-paulino@hotmail.com>
2025-01-05 14:59:15 -0300

    20- Typescript - Creating a Type and Interface of User.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,18 @@
 import {Component, computed, EventEmitter, input, Input, output, Output} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
+// type User = {
+//   id: string
+//   name: string,
+//   avatar: string
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -15,11 +27,7 @@ export class UserComponent {
   // @Input({required: true}) id!: string;
   // @Input({required: true}) avatar!: string
   // @Input({required: true}) name!: string
-  @Input({required: true}) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  }
+  @Input({required: true}) user!: User
   @Output() select = new EventEmitter<string>() // Adding a type, ensure that value passed to select is string
 
   // this output unlike signal, it is another thing
