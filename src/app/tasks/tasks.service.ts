3aa508487a4d11cf082b79b5153f36ccd517485e
Daniel Paulino <daniel-paulino@hotmail.com>
2025-02-07 14:42:57 -0300

    40- Saving tasks in local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -35,6 +35,14 @@ export class TasksService {
     }
   ]
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId:string) {
     console.log("Chegou aqui no srevice")
     return this.tasks.filter((task) => task.userId === userId)
@@ -48,9 +56,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     })
+    this.saveTask()
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTask()
+  }
+
+  private saveTask() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
