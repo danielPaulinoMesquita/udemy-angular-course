c5f5055a365cec129979464b023dd56d0e33f7a9
Daniel Paulino <daniel-paulino@hotmail.com>
2025-02-07 23:55:12 -0300

    42- Declaring and Using Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
+import {HeaderComponent} from "./header/header.component";
+import {TasksComponent} from "./tasks/tasks.component";
+import {UserComponent} from "./user/user.component";
 
 
 @NgModule({
   declarations: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
