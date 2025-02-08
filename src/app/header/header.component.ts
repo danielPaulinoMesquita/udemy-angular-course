8eec4111cb15f49101cff5d9148df462478dd3f7
Daniel Paulino <daniel-paulino@hotmail.com>
2025-02-08 14:03:55 -0300

    43- Migrating all components to use modules

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,7 @@
 import {Component} from "@angular/core";
 
-// Another way to create the decorator @Component
-// @Component({
-//   selector: 'app-header',
-//   standalone: true,
-//   // templateUrl: './header.component.html',
-//   template: '<div><h2>teste com template</h2></div>',
-//   styles: ['h1 { color: red}']
-// })
 @Component({
   selector: 'app-header',
-  standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-import { type User } from './user.model'
+import {type User} from './user.model'
 import {CardComponent} from "../shared/card/card.component";
 
 // type User = {
@@ -11,25 +10,13 @@ import {CardComponent} from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
-  imports: [
-    NgOptimizedImage,
-    CardComponent
-  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // this is not more useful.
-  // @Input({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string
-  // @Input({required: true}) name!: string
   @Input({required: true}) user!: User;
   @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>() ;// Adding a type, ensure that value passed to select is string
-
-  // this output unlike signal, it is another thing
-  // select = output<string>()
 
   get imagePath() {
     return "assets/users/" + this.user.avatar;
